47462accb1049dd187ed6fc9afe76bfd7c699bbf
Rohit <rr5450534@gmail.com>
2026-01-14 00:44:34 +0530

    Add MaxElement In Array

---
+++ MaxElement.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+  int ac[100]={};
+  int n;
+
+  printf("Enter the number of elements to find maximum n= ");
+  scanf("%d",&n);
+  for(int i=0; i<n; i++){
+    printf("Enter element %d: ",i+1);
+    scanf("%d",&ac[i]);
+  }
+
+  for(int i=0; i<n; i++){
+    for(int j=i+1; j<n; j++){
+        if(ac[i]<ac[j]){
+            int temp = ac[i];
+            ac[i]=ac[j];
+            ac[j]=temp;
+        }
+    }
+  }
+  printf("The Maximum Element is: %d",ac[0]);
+   return 0;
+}
