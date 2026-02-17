81165b1754d5efaf850520941fe4efb7bb9ad982
Rohit <rr5450534@gmail.com>
2026-02-17 23:25:52 +0530

    Update the MaxElement

--- MaxElement.c
+++ MaxElement.c
@@ -13,23 +13,24 @@ int main(){
 
   for(int i=0; i<n; i++){
     for(int j=i+1; j<n; j++){
-        if(ac[i]<ac[j]){
+        if(ac[i]>ac[j]){
             int temp = ac[i];
             ac[i]=ac[j];
             ac[j]=temp;
         }
     }
   }
+  
   printf("sorted array is: ");
   for(int i=0; i<n; i++){
     printf("%d ",ac[i]);
   }
 
-  printf("\nsecond maximum element is: %d\n",ac[1]);
-  printf("\nsecond minimum element is: %d\n",ac[n-2]);
+  printf("\nsecond maximum element is: %d\n",ac[n-2]);
+  printf("\nsecond minimum element is: %d\n",ac[1]);
 
-  printf("The Maximum Element is: %d\n",ac[0]);
-  printf("The Minimum Element is: %d",ac[n-1]);
+  printf("The Maximum Element is: %d\n",ac[n-1]);
+  printf("The Minimum Element is: %d",ac[0]);
 
   printf("\nEnter to Exit...");
   getchar();
